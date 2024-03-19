5d235a81b77b48aa9188249949e1876629d48168
Diogo Sousa Silva Lopes <diogodil@hotmail.com>
2024-03-19 10:05:34 -0300

    index

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express")
 const app = express()
+const mysql = require('mysql')
+
+
+const db = mysql.createPool({
+    host: "179.188.16.167",
+    user: "chamadosfacil",
+    password: "Centr0#3127",
+    database: "chamadosfacil"
+})
+
+
 
 
 app.get("/", (req, res)=>{
     res.send("Hello World")
+})
+
+app.get("/getTasks", (req, res) => {
+
+    let SQL = "SELECT * from tasks"
+
+    db.query(SQL, (err, result) => {
+        if (err) console.log(err)
+        else res.send(result)
+    })
 })
 
 app.listen(3001, ()=>{
